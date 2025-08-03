4375caa9284aee524637d8c73161bfc7c100de36
Saurabh Mishra <81604161+sanu321-mishra@users.noreply.github.com>
2025-08-03 18:06:52 +0530

    Remove environment files from tracking and add templates

--- kisanConnectFE/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = { 
-  production: true,
-  googleTranslateApiKey: 'AIzaSyAjjKulUM_jgdWD6jOBCM1kzPKUgpilnZ8'
-};
--- kisanConnectFE/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = { 
-  production: false,
-  googleTranslateApiKey: 'AIzaSyAjjKulUM_jgdWD6jOBCM1kzPKUgpilnZ8'
-};
