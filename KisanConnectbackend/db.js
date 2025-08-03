36188ac58591a675bde0caabe638e5255d10a946
Saurabh Mishra <81604161+sanu321-mishra@users.noreply.github.com>
2025-08-03 17:56:08 +0530

    Update environment configuration: refactor .env file for better organization, add server and JWT settings, and update database connection in db.js to use environment variables. Enhance .gitignore to exclude environment files.

--- KisanConnectbackend/db.js
+++ KisanConnectbackend/db.js
@@ -1,10 +1,13 @@
 // backend/db.js
+require('dotenv').config();
 const { Pool } = require('pg');
+
 const db = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'Saurabh@123',
-  database: 'postgres',
-  port: 5432
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 module.exports = db;
